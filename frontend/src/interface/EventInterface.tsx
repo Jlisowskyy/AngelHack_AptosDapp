410de294aef87dab7f833599c940753789566d3e
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-09-08 16:35:16 +0200

    Added smll comments to interfaces

--- frontend/src/interface/EventInterface.tsx
+++ frontend/src/interface/EventInterface.tsx
@@ -5,10 +5,14 @@ export interface EventInterface {
     location: string; // Location of the event
     image: string; // link to event avatar image
     link: string; // link to the event page
-    ticketsLeft: number; // Number of available tickets from event organizer
-    ticketsTrades: number; // Number of active trades for this event
     price: number; // Initial price of the ticket
     collectionID: string; // Address of the collection
+
+    // Data from blockchain collection
+    ticketsLeft: number; // Number of available tickets from event organizer
+    ticketsTrades: number; // Number of active trades for this event
+
+    // Data from trade saved on blockchain collection
     tradePrice?: number; // Price of the ticket in the trade
     tradeSeller?: string; // Name of seller in the trade
     tradeSellerId?: string; // ID of seller in the trade
