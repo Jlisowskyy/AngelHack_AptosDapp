3277707fe994ba12a4d81539f2f62a045e28cd0a
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2024-09-12 17:00:01 +0200

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
-# Prepare for deployment
+# Setup gcloud
+echo "Listing projects"
 gcloud projects list
-echo "Enter the project ID for deployment"
+echo "Specify the project ID for deployment"
+project_id=""
 read project_id
+if [ -z "$project_id" ]
+then
+    echo "Project ID is empty"
+    exit 1
+fi
+
+gcloud config set project $project_id
+
+# Prepare for deployment
 gcloud app deploy --project=$project_id ./frontend/app.yml
